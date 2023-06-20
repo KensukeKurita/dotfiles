c300e1052b466441e84f7ab9ba7aff3bc7266b9e
KensukeKurita <ovation6868lx@gmail.com>
2023-06-20 22:29:16 +0900

    Setting Mason

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
-# nvim
+# fish
+fish setup
+```bash
+brew install fish
+```
 
+# nvim
 neovim setup
 ```bash
 brew install neovim
 ```
 
+**Plugin Install**
+1. Start neovim
+2. Automatically start installing Packer(PluginManager)
+2. Type `:PackerInstall`
+
+**Set Font**  
+You must use fonts which include icons previewed in sidebar and statusbar.
+1. Get your favorite font via [NerdFont](https://www.nerdfonts.com).
+2. Set the font on your terminal (iTerm2 or others).
+
 ## Config
 ### Set config with lua
 
 ### Plugins
-- [Packer](https://github.com/wbthomason/packer.nvim)
-- [bufferline](https://github.com/akinsho/bufferline.nvim)
+- [Packer](https://github.com/wbthomason/packer.nvim) Plugin Manager
+- [bufferline](https://github.com/akinsho/bufferline.nvim) Edit Tab
 
 ## Reference
 ### Overall
@@ -18,3 +33,4 @@ brew install neovim
 - https://github.com/craftzdog/dotfiles-public
 ### Feature
 - [Neovimで全角スペースをハイライトする](https://zenn.dev/oppara/articles/neovim-highlight-full-width-whitespace)
+- [Neovim+LSPをなるべく簡単な設定で構築する](https://zenn.dev/botamotch/articles/21073d78bc68bf)
---
+++ nvim/plugin/lspconfig.lua
@@ -0,0 +1,55 @@
+-- >> 
+-- Setting Language Server Protocol(LSP)
+-- 
+--  Using below plugins, 
+--   1. nvim-lspconfig
+--   2. mason.nvim
+--      2a. mason-lspconfig.nvim
+--   3. nvim-cmp
+--      3a. cmp-nvim-lsp
+--      3b. vim-vsnip
+      
+local status, mason = pcall(require, "mason")
+if (not status) then
+    print("Error Mason")
+    return
+end
+
+require('mason').setup()
+require('mason-lspconfig').setup_handlers({ function(server)
+	local opt = {
+		capabilities = require('cmp_nvim_lsp').update_capabilities(
+			vim.lsp.protocol.make_client_capabilities()
+			)
+        }
+	require('lspconfig')[server].setup(opt)
+end })
+
+-- << 
+
+
+-- >> build in LSP function
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+-- << build in LSP function
+
+-- >> Completion
+local cmp = require('cmp')
+cmp.setup({
+	snipett = {
+		expand = function(args)
+			vim.fn['vsnip#anonymous'](args.body)
+		end,
+	},
+	sources = {
+		{name = 'nvim_lsp'},
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-p>'] = cmp.mapping.select_prev_item(),
+		['<C-n>'] = cmp.mapping.select_next_item(),
+		['<C-l>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+	}),
+})
+-- << Completion
