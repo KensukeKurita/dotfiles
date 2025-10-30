07fa0be8e95272aec33905ed714ce5d27733a499
KensukeKurita <ovation6868lx@gmail.com>
2025-10-30 10:09:00 +0900

    update wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,30 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+config.automatically_reload_config = true
+-- transparent of window
+config.window_background_opacity = 0.90
+-- remove title bar
+config.window_decorations = "RESIZE"
+-- transparent of tab bar
+config.window_frame = {
+  inactive_titlebar_bg = "none",
+    active_titlebar_bg = "none",
+}
+
+config.audible_bell = 'Disabled'
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 10
+config.color_scheme = 'AdventureTime'
+
+-- Finally, return the configuration to wezterm:
+return config
